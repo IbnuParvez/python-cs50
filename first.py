18bcd94935fe9db3017363c6c21ac5b67eafa502
IbnuParvez <parvezfatma98@gmail.com>
2025-05-27 23:09:24 +0300

    Created function

--- first.py
+++ first.py
@@ -1,4 +1,11 @@
+#create my own function
+
+def hello(to):
+      print("HI,", to)
+
 name = input("What is your name? ").strip().title()
 age = input("What is your age? ").strip()
 
 print(f"Hello {name} who is {age} years old.\nIt should work perfecto")
+
+hello(name)
