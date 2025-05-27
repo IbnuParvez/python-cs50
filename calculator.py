07f8d9d3686c5fc2dccb2f47cad7cb411dccf09b
IbnuParvez <parvezfatma98@gmail.com>
2025-05-27 22:40:19 +0300

    Updated

--- calculator.py
+++ calculator.py
@@ -1,6 +1,4 @@
-a = input("Choose first number: ")
-b = input("Choose second number: ")
+a = int(input("Choose first number: "))
+b = int(input("Choose second number: "))
 
-sum = int(a) + int(b)
-
-print(f"The answer is {sum}")
+print(f"The answer is {a + b}")
