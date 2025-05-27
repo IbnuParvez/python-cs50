20bc892527af07175b4703afe7f327c885ec5278
IbnuParvez <parvezfatma98@gmail.com>
2025-05-27 22:58:43 +0300

    final for today

--- calculator.py
+++ calculator.py
@@ -2,3 +2,8 @@ a = int(input("Choose first number: "))
 b = int(input("Choose second number: "))
 
 print(f"The answer is {a + b}")
+
+c = float(input("Choose first number: "))
+d = float(input("Choose second number: "))
+
+print(f"The answer is {round(c + d)}")
