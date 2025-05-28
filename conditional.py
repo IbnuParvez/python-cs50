8df33924a1534a97d1ad15c85c0dd7c97909288f
IbnuParvez <parvezfatma98@gmail.com>
2025-05-28 15:12:39 +0300

    conditional loops

---
+++ conditional.py
@@ -0,0 +1,10 @@
+a = int(input("Choose your number ").strip())
+
+b = int(input("Choose your second number ").strip())
+
+if a > b:
+    print("Bingo")
+elif a < b:
+    print("Not that Bingo")
+else:
+    print("They must be equal")
